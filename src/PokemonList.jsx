a38247358b6f70260c65a7109234c77bb013ff2d
sush1sui <jplovesraw@gmail.com>
2024-04-04 22:04:50 +0800

    switch case

--- src/PokemonList.jsx
+++ src/PokemonList.jsx
@@ -28,16 +28,33 @@ export default function PokemonList(props) {
             let filtered = pokemonData.filter(pokemon => {
                 return pokemon.name.startsWith(props.searchInput) || pokemon.id.toString().startsWith(props.searchInput);
             });
-            
-            if(props.filter === 'asc-num') {
-                filtered = [...filtered].sort((a, b) => a.id - b.id)
-            } else if (props.filter === 'desc-num') {
-                filtered = [...filtered].sort((a, b) => b.id - a.id)
-            } else if (props.filter === 'asc-name') {
-                filtered = [...filtered].sort((a, b) => a.name.localeCompare(b.name))
-            } else if (props.filter === 'desc-name') {
-                filtered = [...filtered].sort((a, b) => b.name.localeCompare(a.name))
+
+            switch (props.filter) {
+                case 'asc-num':
+                    filtered = [...filtered].sort((a, b) => a.id - b.id)
+                    break;
+                case 'desc-num':
+                    filtered = [...filtered].sort((a, b) => b.id - a.id)
+                    break;
+                case 'asc-name':
+                    filtered = [...filtered].sort((a, b) => a.name.localeCompare(b.name))
+                    break;
+                case 'desc-name':
+                    filtered = [...filtered].sort((a, b) => b.name.localeCompare(a.name))
+                    break;
+                default:
+                    break;
             }
+            
+            // if(props.filter === 'asc-num') {
+            //     filtered = [...filtered].sort((a, b) => a.id - b.id)
+            // } else if (props.filter === 'desc-num') {
+            //     filtered = [...filtered].sort((a, b) => b.id - a.id)
+            // } else if (props.filter === 'asc-name') {
+            //     filtered = [...filtered].sort((a, b) => a.name.localeCompare(b.name))
+            // } else if (props.filter === 'desc-name') {
+            //     filtered = [...filtered].sort((a, b) => b.name.localeCompare(a.name))
+            // }
             setFilteredData(filtered);
         }
     }, [props.searchInput, pokemonData, props.filter]);
