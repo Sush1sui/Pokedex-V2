2717a192420e4bdcd84d65cd13b5df7036df83d2
sush1sui <jplovesraw@gmail.com>
2024-04-07 19:12:09 +0800

    try

--- src/PokemonCard.jsx
+++ src/PokemonCard.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 export default function PokemonCard(props) {
     const [cardClicked, setCardClicked] = useState(0)
+    const [isLoading, setIsLoading] = useState(true)
 
     function capitalizeFirstName(name) {
         return name.charAt(0).toUpperCase() + name.slice(1)
@@ -11,11 +12,14 @@ export default function PokemonCard(props) {
         setCardClicked(prev => prev+1)
     }
 
-    // cardClicked > 0 && console.log(`card click ${props.name}: ${cardClicked}`)
+    function handleImageLoad() {
+        setIsLoading(false)
+    }
 
     return(
         <div className="pokemon--card" onClick={addClick}>
-            <img className="pokemon--card-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={`${props.name}-photo`} />
+            {isLoading && <p>Loading...</p>}
+            <img className="pokemon--card-img" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`} alt={`${props.name}-photo`} onLoad={handleImageLoad} />
             <h5>{`#${props.id} ${capitalizeFirstName(props.name)}`}</h5>
         </div>
     )
